4f497b370d14ffa9b47c1425437ce900bd0ac56f
EthanPassinoFWCS <69813689+EthanPassinoFWCS@users.noreply.github.com>
2021-03-02 15:21:31 -0500

    Add files via upload

---
+++ Lesson 10/Multi-File-Programs/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "my_functions.h"
+// Add declarations here:
+
+int main() {
+    std::cout << is_palindrome("racecar") << "\n";
+    std::cout << tenth_power(3) << "\n";
+    std::cout << average(8.0, 19.0) << "\n";
+}
